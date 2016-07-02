a5b73e3859d9c9af54d3d67711f2b7325a5ae72d
gdufe-junhong <847421525@qq.com>
2016-07-02 11:37:25 +0800

    add simple test in SparkTest

--- src/main/java/spark/SparkContextTest.java
+++ src/main/java/spark/SparkContextTest.java
@@ -1,11 +1,13 @@
 package spark;
 
+import java.util.Date;
+
 /**
  * Created by Administrator on 2016/4/6.
  */
 public class SparkContextTest {
-
+    //add git GUI comment
     public static void main(String[] args) throws  Exception{
-        System.out.println("------Start------");
+        System.out.println(new Date()+"------Start------");
     }
 }
