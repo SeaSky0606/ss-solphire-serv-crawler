1caf1228ca334503e81bb44e0ab92c140f86884b
gdufe-junhong <847421525@qq.com>
2016-07-16 01:37:22 +0800

    generic methor for pagenation

--- src/main/java/org/sun/java/SubListOperation.java
+++ src/main/java/org/sun/java/SubListOperation.java
@@ -18,32 +18,74 @@ public class SubListOperation {
 		}
 		return lists;
 	}
-	
+
 	/**
 	 * 
-	 * @param pageNum
-	 * @param pageSize
-	 * @return  简单分页功能
+	 * @param size
+	 * @return 初始化用户列表
 	 */
-	public List<String> subList(int pageNum, int pageSize) {
-		List<String> lists = init(COUNT);
+	public List<User> initUser(int size) {
+		List<User> lists = new ArrayList<User>();
+		for (int i = 0; i < size; i++) {
+			lists.add(new User((i + 1), "user" + (i + 1)));
+		}
+		return lists;
+	}
+
+	
+
+	/**
+	 * 
+	 * @param lists 将被分页展示的列表
+	 * @param pageNum 页码数
+	 * @param pageSize 每页条数大小
+	 * @return
+	 */
+	public <T> List<T> generalPagenation(List<T> lists,int pageNum, int pageSize) {
 		int fromIndex = (pageNum - 1) * pageSize;
 		int toIndex = fromIndex + pageSize;
-		return lists.subList(fromIndex, toIndex);
+		int size = lists.size();
+		if (fromIndex >= size) {
+			return new ArrayList<T>();
+		}
+		return lists.subList(fromIndex, Math.min(size, toIndex));
+	}
+	
+
+	static class User {
+		int id;
+		String name;
+
+		public User(int id, String name) {
+			super();
+			this.id = id;
+			this.name = name;
+		}
+
+		@Override
+		public String toString() {
+			return "User [id=" + id + ", name=" + name + "]";
+		}
+
 	}
 
 	public static void main(String[] args) {
 		System.out.println("-------- start --------");
-		new SubListOperation().output();
+		new SubListOperation().outputT();
 		System.out.println("-------- finish --------");
 	}
-	
-	public void output(){
-		int pageNum=2;
-		int pageSize=5;
-		for(String s:subList(pageNum,pageSize)){
+
+	public void outputT() {
+		int pageNum = 3;
+		int pageSize = 5;
+		for (Object s : generalPagenation(init(18),pageNum, pageSize)) {
+			System.out.println(s);
+		}
+		System.out.println("---------***---------");
+		for (Object s : generalPagenation(initUser(12),pageNum, pageSize)) {
 			System.out.println(s);
 		}
 	}
+
 
 }
