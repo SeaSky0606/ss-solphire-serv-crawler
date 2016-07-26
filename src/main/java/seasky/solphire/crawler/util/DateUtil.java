39e0c9bac00893e3749197462a07ddf07792e972
gdufe-junhong <847421525@qq.com>
2016-07-26 23:45:54 +0800

    add mthord for date(Time) comparing

--- src/main/java/seasky/solphire/crawler/util/DateUtil.java
+++ src/main/java/seasky/solphire/crawler/util/DateUtil.java
@@ -1,14 +1,33 @@
 package seasky.solphire.crawler.util;
 
+import java.sql.Time;
+import java.util.Date;
+
 public class DateUtil {
-	
-	public static long getCurrentTime(){
+
+	public static long getCurrentTime() {
 		return System.currentTimeMillis();
 	}
-
+	/* output=
+		23:44:27
+		-1
+	*/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(DateUtil.getCurrentTime());
+		String s1 = "20:00:01";
+		String s2 = "22:00:01";
+		System.out.println(new Time(System.currentTimeMillis()));
+		Time time1 = Time.valueOf(s1);
+		Time time2 = Time.valueOf(s2);
+		System.out.println(time1.compareTo(time2));
+		
+	}
+
+	public static boolean dateCompare(Date d1, Date d2) {
+		return d1.compareTo(d2) > 0;
+	}
+
+	public static boolean timeCompare(Time t1, Time t2) {
+		return t1.compareTo(t2) > 0;
 	}
 
 }
