8558bff3797f4ef5638aac7a6ac1849d82c226c4
junhong <847421525@qq.com>
2016-08-04 13:44:10 +0800

    add test for restfulapi 【new Edition】

--- src/test/java/restful/juhe/JuheRestfulTest.java
+++ src/test/java/restful/juhe/JuheRestfulTest.java
@@ -4,8 +4,7 @@ import org.junit.After;
 import org.junit.Test;
 
 /**
- * Created by junhong
- * on 2016/8/1:22:28 @ 22:28
+ * Created by junhong on 2016/8/1.
  */
 public class JuheRestfulTest {
 
@@ -24,9 +23,10 @@ public class JuheRestfulTest {
     @Test
     public void testFetchResultWithPost() throws Exception {
         String request = "http://japi.juhe.cn/askanswer/getTypes?key=1ec691c914a6111e27f0b04cbaef5dd6";
-        request = "http://japi.juhe.cn/funny/search.from?st=0&count=10&term=wife&key=9fa7c5b46a6e8b2281cf0f6db2f6c1d0";
-        request = "http://192.168.33.10:17575/getMovies?ids=196613_199602";
-        String result = JuheRestful.fetchResult(request, "POST");
+        request = "http://centos:9200/_plugin/head/";
+        request = "http://192.168.33.10:17575/getMovieDetailById?id=196613";
+        String result = JuheRestful.fetchResult(request, "GET");
+
         System.out.println("result=" + result);
     }
 
@@ -36,15 +36,6 @@ public class JuheRestfulTest {
         String key = "9fa7c5b46a6e8b2281cf0f6db2f6c1d0";
         String request = "http://japi.juhe.cn/funny/search.from?st=0&count=10&term=%s&key=%s";
         System.out.println(JuheRestful.fetchResult(String.format(request, query, key)));
-
-    }
-
-    @Test
-    public void testGetFetchResultBytes() throws Exception {
-        String query = "wife";
-        String key = "9fa7c5b46a6e8b2281cf0f6db2f6c1d0";
-        String url = "http://192.168.33.10:17575/getMovies?ids=196613_199602";
-        System.out.println(new String(JuheRestful.fetchResultBytes(url)));
 
     }
 }
